3a771186f6318895c1e5acaf09fe3555b6be371c
Joseph Chingalo <profschingalo@gmail.com>
2016-11-28 11:27:39 +0300

    vehicle verification et data as well as business licence history data ready for display

--- src/pages/vehicle-search/vehicle-search.ts
+++ src/pages/vehicle-search/vehicle-search.ts
@@ -3,6 +3,7 @@ import { NavController,MenuController,ToastController } from 'ionic-angular';
 import {User} from "../../providers/user";
 import {Program} from "../../providers/program";
 import {SqlLite} from "../../providers/sql-lite";
+import {EventProvider} from "../../providers/event-provider";
 
 /*
   Generated class for the VehicleSearch page.
@@ -13,7 +14,7 @@ import {SqlLite} from "../../providers/sql-lite";
 @Component({
   selector: 'page-vehicle-search',
   templateUrl: 'vehicle-search.html',
-  providers : [User,Program,SqlLite]
+  providers : [User,Program,SqlLite,EventProvider]
 })
 export class VehicleSearchPage {
 
@@ -26,20 +27,28 @@ export class VehicleSearchPage {
   public businessLicenceProgram : any;
   public businessLicenceProgramName : string;
 
+  public businessLicenceVehicleDataElementId : string;
 
   //vehicle program
   public vehicleProgram : any;
   public vehicleProgramName : string;
   public vehiclePlateNumber :string;
   public searchCriteria :string = "Vehicle Plate Number/Registration Number";
+  public searchCriteriaDataElement :any;
+  public dataElementIdToNameMapping : any = {};
+
+  //search results :
+  public vehicleData : any;
+  public vehicleBusinessLicenceHistory : any;
+
 
   constructor(public navCtrl: NavController,public menuCtrl: MenuController,
-              public Program : Program,public SqlLite:SqlLite,
+              public Program : Program,public SqlLite:SqlLite,public EventProvider : EventProvider,
               public user : User,public toastCtrl: ToastController) {
 
     this.menuCtrl.enable(true);
     this.vehicleProgramName = "Vehicle";
-    this.businessLicenceProgram = "Business License History";
+    this.businessLicenceProgramName = "Business License History";
     this.user.getCurrentUser().then(user=>{
       this.currentUser = user;
       this.loadVehicleMetadata();
@@ -52,6 +61,13 @@ export class VehicleSearchPage {
     this.setLoadingMessages("Loading vehicle metadata");
     this.Program.getProgramByName(this.vehicleProgramName,this.currentUser).then((program:any)=>{
       this.vehicleProgram = program;
+      program.programStages[0].programStageDataElements.forEach((programStageDataElement :any)=>{
+        if(programStageDataElement.dataElement.name.toLowerCase() == this.searchCriteria.toLowerCase()){
+          this.searchCriteriaDataElement = programStageDataElement.dataElement;
+        }
+        this.dataElementIdToNameMapping[programStageDataElement.dataElement.id] = programStageDataElement.dataElement.name;
+      });
+
       this.loadBusinessLicenceMetadata();
     },error=> {
       this.loadingData = false;
@@ -63,6 +79,13 @@ export class VehicleSearchPage {
     this.setLoadingMessages("Loading business licence metadata");
     this.Program.getProgramByName(this.businessLicenceProgramName,this.currentUser).then((program:any)=>{
       this.businessLicenceProgram = program;
+      let dataElementName  = "Program_Vehicle";
+      program.programStages[0].programStageDataElements.forEach((programStageDataElement :any)=>{
+        if(programStageDataElement.dataElement.name.toLowerCase() == dataElementName.toLowerCase()){
+          this.businessLicenceVehicleDataElementId = programStageDataElement.dataElement.id;
+        }
+      });
+
       this.loadingData = false;
     },error=>{
       this.loadingData=false;
@@ -71,7 +94,50 @@ export class VehicleSearchPage {
   }
 
   search(){
-    this.setToasterMessage('ready to search');
+    if(this.vehiclePlateNumber){
+      this.vehiclePlateNumber = this.vehiclePlateNumber.toUpperCase();
+      if(this.vehiclePlateNumber.length == 7){
+        this.vehiclePlateNumber =  this.vehiclePlateNumber.substr(0,4) + ' ' + this.vehiclePlateNumber.substr(4);
+      }
+      if(this.searchCriteriaDataElement.id){
+        this.loadSearchingResult();
+      }else{
+        this.setToasterMessage('Fail to set relation data element ');
+      }
+    }else{
+      this.setToasterMessage('Please enter Vehicle Plate Number');
+    }
+  }
+
+  loadSearchingResult(){
+    this.loadingMessages = [];
+    this.loadingData = true;
+    this.setLoadingMessages("Please wait,while search for a vehicle");
+    this.EventProvider.findEventsByDataValue(this.searchCriteriaDataElement.id,this.vehiclePlateNumber,this.vehicleProgram.id,this.currentUser).then((events:any)=>{
+      if(events.length > 0){
+        this.vehicleData = events[0];
+        this.setLoadingMessages("Loading business licence history");
+        this.EventProvider.findEventsByDataValue(this.businessLicenceVehicleDataElementId,events[0].event,this.businessLicenceProgram.id,this.currentUser).then((events:any)=>{
+          if(events.length > 0){
+            this.vehicleBusinessLicenceHistory = events;
+            this.loadingData = false;
+          }else{
+            this.loadingData = false;
+            this.setToasterMessage("business licence history for this vehicle has been not found");
+          }
+        },error=>{
+          this.loadingData = false;
+          this.setToasterMessage("Fail to load business licence history");
+        });
+      }else{
+        this.loadingData = false;
+        this.setToasterMessage("vehicle has not been found");
+      }
+
+    },error=>{
+      this.loadingData = false;
+      this.setToasterMessage("Fail to search vehicle");
+    });
   }
 
   ionViewDidLoad() {};
